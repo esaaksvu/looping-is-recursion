c0af2b27bf2878260708e9d88cd1348e4bcdc78d
Eero Sääksvuori <esaaksvu@cs.helsinki.fi>
2013-10-26 05:14:18 +0300

    looping is... 5/8

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,22 +1,64 @@
 (ns looping-is-recursion)
 
-(defn power [base exp]
-  ":(")
+(defn power [n k]
+  (let [helper (fn[pow res]
+       (cond
+         (= n 0) 0
+         (= k 0) 1
+         (= pow 1) res
+         :else (recur (dec pow) (* n res))))]
+    (helper k n)))
 
 (defn last-element [a-seq]
-  ":(")
+   (let [help (fn [a]
+        (if (empty? (rest a))
+          (first a)
+            (recur (rest a))))]
+     (help a-seq)))
+
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (let [help (fn [a b]
+        (cond
+          (and (empty? a) (empty? b)) true
+          (not (= (count a) (count b))) false
+          (not (= (first a) (first b))) false
+          :else (recur (rest a) (rest b))))]
+    (help seq1 seq2)))
 
 (defn find-first-index [pred a-seq]
-  ":(")
+  (if (empty? a-seq)
+    nil
+      (loop [indeksi 0
+             sequenc a-seq]
+        (cond
+          (pred (first sequenc)) indeksi
+          (empty? (rest sequenc)) nil
+          :else (recur (inc indeksi) (rest sequenc))))))
+
 
 (defn avg [a-seq]
-  -1)
+  (loop [i 1
+         s a-seq
+         sum 0]
+    (cond
+      (empty? (rest s)) (/ (+ (first s) sum) i)
+      :else (recur (inc i) (rest s) (+ (first s) sum)))))
+
+
+(defn toggle [a-set elem]
+  (if(contains? a-set elem)
+    (disj a-set elem)
+      (conj a-set elem)))
+
 
 (defn parity [a-seq]
-  ":(")
+  (loop [odds (set ())
+         s a-seq]
+    (cond
+      (empty? s) odds
+      :else (recur (toggle odds (first s)) (rest s)) )))
+
 
 (defn fast-fibo [n]
   ":(")
